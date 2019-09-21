abc5bdda3596fe9fcc90d4fd1403c2a3fb5fe7c5
Froylan Miguel Diaz Quintano <huracan_dq95feb@hotmail.com>
2019-09-21 17:01:05 -0500

    modificacion en el formulario
    Se modifico la parte de la carag del fromulario

--- editar.php
+++ editar.php
@@ -14,24 +14,7 @@
         <legend>Edite el contacto</span></legend>
 
     <!--se crean los campos de los formularios-->
-        <div class="campos">
-            <div class="campo">
-                <label for="nombre">Nombre:</label>
-                <input type="text" placeholder="Nombre contacto" name="nombre" id="nombre">
-            </div>
-            <div class="campo">
-                <label for="empresa">Empresa:</label>
-                <input type="text" placeholder="Nombre empresa" name="empresa" id="empresa">
-            </div>
-            <div class="campo">
-                <label for="telefono">Teléfono:</label>
-                <input type="tel" placeholder="Telefono" name="telefono" id="telefono">
-            </div>
-            
-        </div>
-        <div class="campo enviar">
-            <input type="submit" value="Añadir">
-        </div>
+    <?php include 'inc/layout/formulario.php'; ?>
 
 
         </form>
---
+++ inc/layout/formulario.php
@@ -0,0 +1,18 @@
+<div class="campos">
+            <div class="campo">
+                <label for="nombre">Nombre:</label>
+                <input type="text" placeholder="Nombre contacto" name="nombre" id="nombre">
+            </div>
+            <div class="campo">
+                <label for="empresa">Empresa:</label>
+                <input type="text" placeholder="Nombre empresa" name="empresa" id="empresa">
+            </div>
+            <div class="campo">
+                <label for="telefono">Teléfono:</label>
+                <input type="tel" placeholder="Telefono" name="telefono" id="telefono">
+            </div>
+            
+        </div>
+        <div class="campo enviar">
+            <input type="submit" value="Añadir">
+        </div>
